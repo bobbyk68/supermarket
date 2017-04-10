c6f33256ec27d348a2c7a94f36f802038f55f9c3
bobbyk68 <bobbyk68@gmail.com>
2017-04-10 22:52:57 +0100

    test 2

--- supermarket/src/test/java/com/akk/supermarket/SuperMarketTest.java
+++ supermarket/src/test/java/com/akk/supermarket/SuperMarketTest.java
@@ -1,13 +1,17 @@
 package com.akk.supermarket;
 import java.math.BigDecimal;
 
-import junit.framework.Assert;
-
 import org.junit.Test;
 
 import com.akk.supermarket.domain.Basket;
+import com.akk.supermarket.domain.IProduct;
+import com.akk.supermarket.domain.Product;
+import com.akk.supermarket.domain.UnitPrice;
+import com.akk.supermarket.domain.WeightedPrice;
 import com.akk.supermarket.factory.SuperMarketFactory;
 import com.akk.supermarket.service.CheckoutService;
+
+import junit.framework.Assert;
 
 public class SuperMarketTest {
 	
@@ -19,13 +23,13 @@ public class SuperMarketTest {
 		Assert.assertTrue(price.compareTo(BigDecimal.ZERO) == 0);
 	}
 	
-	/*
+	
 	@Test
 	public void testCombinedProductTypes() {
-		Product potatoes = new Product("Potato", "King Edwards", new WeightedPrice(1.99));
-		Product apple = new Product("Apple", "Granny", new UnitPrice(0.59));
+		IProduct potatoes = new Product("Potato", "King Edwards", new WeightedPrice(1.99));
+		IProduct apple = new Product("Apple", "Granny", new UnitPrice(0.59));
 	}
-	
+	/*
 	@Test
 	public void testBuyOneGetOneFree() {
 		ISpecialOffers bogof = new BuyOneGetOneFree();
